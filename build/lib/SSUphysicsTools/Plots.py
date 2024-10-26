f974031c7feac2e352a1aa5758a658a1c448bb34
kty1004 <ktyoung1004@gmail.com>
2024-10-26 17:05:24 +0900

    build: build files 0.3.0

--- build/lib/SSUphysicsTools/Plots.py
+++ build/lib/SSUphysicsTools/Plots.py
@@ -3,6 +3,7 @@ import numpy as np
 from pandas import DataFrame
 from typing import Optional
 import matplotlib.pyplot as plt
+import os
 
 class Plots(get_all_csv_paths):
     def __init__(self,plot_name:tuple[str], flatten:Optional[bool]=False,directory:Optional[str]='data'):
@@ -11,6 +12,12 @@ class Plots(get_all_csv_paths):
         '''
         super().__init__(flatten=flatten,directory=directory)
         self.plot_name = plot_name
+        
+        # plot 이미지를 저장할 디렉토리 생성
+        directory = 'fig'
+        # 디렉토리가 없으면 생성
+        if not os.path.exists(directory):
+            os.makedirs(directory)
     
     # plots
     def orginal_data_plots(self, is_save:Optional[bool]=False,dpi:Optional[int]=300):
