32989406f65743ff108e4f375f24073140d76dfa
kty1004 <ktyoung1004@gmail.com>
2024-10-26 14:20:54 +0900

    Chore: add short discription to setup.py

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ setup(
     ],
     author="Taeyoung Kim",
     author_email="rocketman@soongsil.ac.kr",
-    description="A short description of the package",
+    description="숭실대학교 물리학과 실험에 사용되는 데이터 처리 및 시각화 도구",
     long_description=open('README.md').read(),
     long_description_content_type='text/markdown',
     url="https://github.com/kty1004/SSUphysicsExperiment_Supporting_tools?tab=readme-ov-file#readme",
