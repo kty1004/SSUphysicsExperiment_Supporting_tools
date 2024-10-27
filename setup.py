0b51bb9ccdf63d48c2a3dcd0a4370e0b9462b423
kty1004 <ktyoung1004@gmail.com>
2024-10-27 19:45:00 +0900

    Update package version to 2.0.5
    - Fix Plot class to goal of data plot

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="SSUphysicsTools",
-    version="2.0.3",
+    version="2.0.5",
     packages=find_packages(),
     install_requires=[
         "numpy",
